6cf2f5a1432f9201ff255627e6261c00423deeff
n1kh1lmore <nikupcm10@gmail.com>
2022-08-19 10:21:49 -0800

    insetion sort

--- JAVA_Lecture/sorting.java
+++ JAVA_Lecture/sorting.java
@@ -44,20 +44,38 @@ public class sorting {
         // places the elements in increasing order.
         // Time complexity: O(N2)
 
-        for(int i =0; i<arr.length-1; i++){
+        for (int i = 0; i < arr.length - 1; i++) {
             int smallest = i;
-            for(int j = i+1; j<arr.length; j++){
-                if(arr[j] < arr[smallest]){
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[smallest]) {
                     smallest = j;
 
                 }
             }
-            //swap
+            // swap
             int temp = arr[smallest];
             arr[smallest] = arr[i];
             arr[i] = temp;
         }
         System.out.println("for selection sort");
+        printArray(arr);
+
+        // Insertion Sort
+        // Idea: Take an element from the unsorted array, place it in its corresponding
+        // position in the sorted part, and shift the elements accordingly.
+        // Time Complexity: O(N2)
+
+        for(int i=1; i<arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+                while(j >= 0 && arr[j] > current) {
+                    //Keep swapping
+                    arr[j+1] = arr[j];
+                    j--;
+                }
+            arr[j+1] = current;
+        }
+        System.out.println("for insertion sort");
         printArray(arr);
 
 
