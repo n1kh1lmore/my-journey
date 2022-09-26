073f67ada3b5b10b627cb84f289bd3a9ef2d49c2
n1kh1lmore <nikupcm10@gmail.com>
2022-09-26 22:19:16 +0530

    remove duplicates part-2

--- JAVA_Lecture/recursions.java
+++ JAVA_Lecture/recursions.java
@@ -154,8 +154,16 @@ public class recursions {
 
     // Remove duplicates in a string.
     public static String removeDuplicates(String str, int idx, boolean present[]) {
-        if (idx == str.length()) {
+        if(idx == str.length()){
             return "";
+
+        }
+        char curr = str.charAt(idx);
+        if(present[curr-'a']) {
+            return removeDuplicates(str, idx+1, present);
+        } else {
+            present[curr-'a'] = true;
+            return curr + removeDuplicates(str, idx+1, present);
         }
     }
 
