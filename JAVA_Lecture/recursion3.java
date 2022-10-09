a6999c9f2a27221e8c30f189e28d0c8bd4dbb9f1
n1kh1lmore <nikupcm10@gmail.com>
2022-10-09 11:51:53 +0530

    count path maze

--- JAVA_Lecture/recursion3.java
+++ JAVA_Lecture/recursion3.java
@@ -1,24 +1,46 @@
+
+
 public class recursion3 {
     // Advanced 
 
     //print all the permutation of a string 
-    public static void printPermutation(String str, int idx, String perm) {
-        if(str.length() == 0){
-            System.out.println(perm);
-            return;
+    // public static void printPermutation(String str, int idx, String perm) {
+    //     if(str.length() == 0){
+    //         System.out.println(perm);
+    //         return;
+    //     }
+
+    //     for(int i = 0; i<str.length(); i++){
+    //         char currChar = str.charAt(i);
+    //         String newStr = str.substring(0, i) + str.substring(i+1);
+    //         printPermutation(newStr, idx+1, perm+currChar);
+    //     }
+        
+    // }
+
+
+    // count path maze
+
+    public static int coutnPath(int i, int j, int m, int n){
+        if(i == m-1 || j == n-1) {
+            return 1;
         }
 
-        for(int i = 0; i<str.length(); i++){
-            char currChar = str.charAt(i);
-            String newStr = str.substring(0, i) + str.substring(i+1);
-            printPermutation(newStr, idx+1, perm+currChar);
-        }
-        
+        return coutnPath(i +1, j, m, n) + coutnPath(i, j+1, m, n);
     }
 
 
     public static void main(String[] args) {
-        String str = "abc";
-        printPermutation(str, 0, "");
+
+        //print all permutations
+        // String str = "abc";
+        // printPermutation(str, 0, "");
+
+
+        // count path maze
+        int m = 4;
+        int n = 5;
+        System.out.println(coutnPath(0, 0, m, n));
+
     }
 }
