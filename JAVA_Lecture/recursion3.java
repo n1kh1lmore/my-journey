745215bde0c11be8844aa8d73e665f600c4ccac0
n1kh1lmore <nikupcm10@gmail.com>
2022-10-09 11:56:24 +0530

    tiling problem

--- JAVA_Lecture/recursion3.java
+++ JAVA_Lecture/recursion3.java
@@ -1,46 +1,60 @@
 
-
 public class recursion3 {
-    // Advanced 
+    // Advanced
 
-    //print all the permutation of a string 
+    // print all the permutation of a string
     // public static void printPermutation(String str, int idx, String perm) {
-    //     if(str.length() == 0){
-    //         System.out.println(perm);
-    //         return;
-    //     }
-
-    //     for(int i = 0; i<str.length(); i++){
-    //         char currChar = str.charAt(i);
-    //         String newStr = str.substring(0, i) + str.substring(i+1);
-    //         printPermutation(newStr, idx+1, perm+currChar);
-    //     }
-        
+    // if(str.length() == 0){
+    // System.out.println(perm);
+    // return;
     // }
 
+    // for(int i = 0; i<str.length(); i++){
+    // char currChar = str.charAt(i);
+    // String newStr = str.substring(0, i) + str.substring(i+1);
+    // printPermutation(newStr, idx+1, perm+currChar);
+    // }
+
+    // }
 
     // count path maze
 
-    public static int coutnPath(int i, int j, int m, int n){
-        if(i == m-1 || j == n-1) {
+    // public static int coutnPath(int i, int j, int m, int n){
+    // if(i == m-1 || j == n-1) {
+    // return 1;
+    // }
+
+    // return coutnPath(i +1, j, m, n) + coutnPath(i, j+1, m, n);
+    // }
+
+
+
+    // tiling problem
+
+    public static int placeTiles(int n, int m) {
+        if(n<m){
             return 1;
+
+        }else if(n == m){
+            return 2;
         }
-
-        return coutnPath(i +1, j, m, n) + coutnPath(i, j+1, m, n);
+        return placeTiles(n-1, m) + placeTiles(n-m, m);      
     }
-
 
     public static void main(String[] args) {
 
-        //print all permutations
+        // print all permutations
         // String str = "abc";
         // printPermutation(str, 0, "");
 
-
         // count path maze
-        int m = 4;
-        int n = 5;
-        System.out.println(coutnPath(0, 0, m, n));
+        // int m = 4;
+        // int n = 5;
+        // System.out.println(coutnPath(0, 0, m, n));
 
+
+        //tiling problem
+        int n = 4, m = 4;
+        System.out.println(placeTiles(n, m));
     }
 }
